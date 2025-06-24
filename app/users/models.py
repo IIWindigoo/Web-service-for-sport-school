3fa7ee68faf2fae50fcbe2901472e67181f9000d
Ruslan B <rusbez04@yandex.ru>
2025-06-24 17:04:51 +0500

    описана модель пользователей и их роли

---
+++ app/users/models.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+from sqlalchemy import Enum as SqlEnum
+from sqlalchemy.orm import Mapped, mapped_column
+from app.database import Base, int_pk, str_uniq
+
+
+class UserRole(str, Enum):
+    client = "client"
+    admin = "admin"
+    trainer = "trainer"
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int_pk]
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    phone_number: Mapped[str_uniq]
+    email: Mapped[str_uniq]
+    password: Mapped[str]
+    role: Mapped[UserRole] = mapped_column(SqlEnum(UserRole), default=UserRole.client)
