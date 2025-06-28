f1014c665ef69f27dfdf01636393e4f63186f860
Ruslan B <rusbez04@yandex.ru>
2025-06-28 17:18:58 +0500

    Добавлены роутеры для переходов по страницам сайта

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,14 +1,21 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 from datetime import date, time, datetime, timezone
 from typing import Any
 import locale
 
-from app.trainings.router import get_trainings
+from app.trainings.router import get_my_trainings, get_trainings
+from app.trainings.models import Training
 from app.booking.router import get_user_bookings
-from app.users.dependencies import get_current_user_or_none
-from app.users.models import User
+from app.users.dependencies import role_required
+from app.users.schemas import SUserFilter
+from app.users.models import UserRole, User
+from app.users.dao import UserDAO
+from app.users.router import get_all_users
+from app.database import async_session
 
 
 locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
@@ -27,20 +34,39 @@ def format_time(value: time):
 def render_template(request: Request, template_name: str, context: dict[str, Any]):
     context["request"] = request
     context["user"] = getattr(request.state, "user", None)
+    context["now"] = datetime.now(timezone.utc).timestamp()
     return templates.TemplateResponse(template_name, context)
 
 templates.env.filters["format_date"] = format_date
 templates.env.filters["format_time"] = format_time
 
 @router.get("/")
-async def home_page(request: Request, 
-                    trainings=Depends(get_trainings)):
+async def home_page(request: Request):
+    async with async_session() as session:
+        query = select(Training).options(joinedload(Training.trainer))
+        result = await session.execute(query)
+        trainings = result.unique().scalars().all()
+
+        trainers = []
+        if request.state.user and request.state.user.role == UserRole.admin:
+            trainers = await UserDAO.find_all(SUserFilter(role=UserRole.trainer))
+
+        return render_template(request,
+                            template_name="index.html",
+                            context={
+                                    "trainings": trainings,
+                                    "trainers": trainers,
+                                    })
+
+@router.get("/me_trainer/")
+async def get_me_trainer_page(request: Request,
+                      trainings=Depends(get_my_trainings),
+                      user_data: User = Depends(role_required([UserRole.trainer]))):
     return render_template(request,
-                           template_name="index.html",
+                           template_name="account_trainer.html",
                            context={
-                                "trainings": trainings,
-                                "now": datetime.now(timezone.utc).timestamp(),
-                                })
+                               "trainings": trainings,
+                           })
 
 @router.get("/me/")
 async def get_me_page(request: Request,
@@ -49,5 +75,23 @@ async def get_me_page(request: Request,
                            template_name="account.html",
                            context={
                                "bookings": bookings,
-                               "now": datetime.now(timezone.utc).timestamp(),
+                           })
+
+@router.get("/me_admin")
+async def get_me_admin_page(request: Request,
+                      users=Depends(get_all_users),
+                      user_data: User = Depends(role_required([UserRole.admin]))):
+    
+    # Получаем все тренировки с тренерами
+    async with async_session() as session:
+        query = select(Training).options(joinedload(Training.trainer))
+        result = await session.execute(query)
+        trainings = result.unique().scalars().all()
+
+    return render_template(request,
+                           template_name="account_admin.html",
+                           context={
+                               "trainings": trainings,
+                               "users": users,
+                               "current_user_id": user_data.id
                            })
